5dd8319c0ce2e785b52983c837602e79965b2b57
Brandon-Gower-Winter <bgwsa101@gmail.com>
2018-07-19 11:12:27 +0200

    Initial Commit. Setup player movement and imported assets.

---
+++ CSC3020H_ASSIGNMENT4/Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public CameraMode mode;
+    [Header("First Person Properties")]
+    public Transform fp_camera_tranform;
+    public Transform fp_player_tranform;
+    public float fp_horizontal_rotation_speed;
+    public float fp_vertical_rotation_speed;
+    public Vector2 vertical_clamp;
+
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+	    switch(mode)
+        {
+            case CameraMode.FIRSTPERSON:
+                manageFP();
+                break;
+            case CameraMode.THIRDPERSON:
+                manageTP();
+                break;
+            case CameraMode.ORBIT:
+                manageOrbit();
+                break;
+        }
+	}
+
+    private void manageOrbit()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void manageTP()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void manageFP()
+    {
+        //We want to rotate the entire players body with horizontal movement
+        fp_player_tranform.Rotate(new Vector3(0, Input.GetAxis("Mouse X") * fp_horizontal_rotation_speed));
+        //We want to rotate only the gun and camera with vertical movement
+        fp_camera_tranform.Rotate(new Vector3(Input.GetAxis("Mouse Y") * -fp_vertical_rotation_speed,0));
+        Vector3 newAngle = fp_camera_tranform.eulerAngles;
+        newAngle.x = clampAngle(fp_camera_tranform.eulerAngles.x,vertical_clamp.x,vertical_clamp.y);
+        fp_camera_tranform.eulerAngles = newAngle;
+    }
+
+    private float clampAngle(float angle, float from, float to)
+    {
+        // accepts e.g. -80, 80
+        if (angle < 0f) angle = 360 + angle;
+        if (angle > 180f) return Mathf.Max(angle, 360 + from);
+        return Mathf.Min(angle, to);
+    }
+}
+
+public enum CameraMode
+{
+    FIRSTPERSON,
+    THIRDPERSON,
+    ORBIT
+}
---
+++ CSC3020H_ASSIGNMENT4/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour {
+
+    [Header("Auto set at runtime")]
+    public Rigidbody player_rigid;
+
+    [Header("Speed Values")]
+    public float forward_speed;
+    public float forward_speed_clamp;
+    public float backward_speed;
+    public float backward_speed_clamp;
+    public float sideways_speed;
+    public float sideways_speed_clamp;
+
+    [Header("Animation Properties")]
+    public Animator player_animations;
+
+    void Start ()
+    {
+        player_rigid = GetComponent<Rigidbody>();
+        player_animations = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        player_animations.SetBool("IsWalking",manageMovement());
+	}
+
+    bool manageMovement()
+    {
+        bool isMoving = false;
+        Vector3 movement_result = new Vector3();
+
+        //Manage left and right
+        if(Input.GetKey(KeyCode.A))
+        {
+            isMoving = true;
+            movement_result += Vector3.left * sideways_speed;
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            isMoving = true;
+            movement_result +=  Vector3.right * sideways_speed;
+        }
+
+        //Manage Forward and Backwards
+        if (Input.GetKey(KeyCode.W))
+        {
+            isMoving = true;
+            movement_result += Vector3.forward * forward_speed;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            isMoving = true;
+            movement_result += Vector3.back * backward_speed;
+        }
+
+        if(!isMoving)
+        {
+            //If the player hasn't move set their veclocity to 0. This will stop players immediately when the move their hands off the keys
+            player_rigid.velocity = new Vector3(0, player_rigid.velocity.y);
+            return false;
+        }
+
+        movePlayer(movement_result);
+        clampSpeed();
+        return true;
+    }
+
+    void movePlayer(Vector3 force)
+    {
+        player_rigid.AddRelativeForce(force);
+    }
+
+    void clampSpeed()
+    {
+
+        float zVal = Mathf.Clamp(player_rigid.velocity.z, -backward_speed, forward_speed_clamp);
+        float xVal = Mathf.Clamp(player_rigid.velocity.x, -sideways_speed_clamp, sideways_speed_clamp);
+
+        player_rigid.velocity = new Vector3(xVal, player_rigid.velocity.y, zVal);
+
+    }
+}
